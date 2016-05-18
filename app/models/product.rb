efb2b8c8f29c5e2b88f2f98cb31e751bc6d28a4a
Justin Davidson <jutstindavidson23@gmail.com>
2016-05-18 12:42:16 +1000

    Added caching and scaffold for cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,8 @@ class Product < ApplicationRecord
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'Must be a URL for GIF, JPG or PNG image.'
   }
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
